7eac175bf29857134828d1aa4e70f18dd8e20e7b
Nguyen Ngo <mnguyenngo@gmail.com>
2018-08-16 21:59:22 -0700

    Added company database

--- rest-api/api.py
+++ rest-api/api.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_restful import Api, Resource
 # reqparse,
 # abort,
-
+from pymongo import MongoClient
 import json
+# from bson import json_util
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,6 +12,14 @@ api = Api(app)
 
 with open('../data/commercial.json', 'r') as f:
     PERMITS = json.load(f)
+
+mc = MongoClient('localhost', 27017)
+db = mc['builtby']
+COMPANIES = list(db['companies_20180816'].find())
+
+# convert mongo objectid object to string for json serialization
+for company in COMPANIES:
+    company['_id'] = str(company['_id'])
 
 
 # shows a list of all permits
@@ -30,9 +39,27 @@ class PermitList(Resource):
         return PERMITS
 
 
+# shows a list of all permits
+class Companies(Resource):
+    def get(self):
+        """Return the current TODO dictionary
+
+        Example:
+            # In the terminal
+            $ curl http://localhost:5000/companies
+
+            OR
+
+            # Python
+            requests.get('http://localhost:5000/companies').json()
+        """
+        return COMPANIES
+
+
 # Setup the Api resource routing here
 # Route the URL to the resource
 api.add_resource(PermitList, '/')
+api.add_resource(Companies, '/companies')
 
 
 if __name__ == '__main__':
